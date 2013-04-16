b33710ac0eae506d0a104a52501dce696eb9b30d
ksedge <kieran.sedgwick@gmail.com>
2013-04-16 18:31:43 -0400

    Finished most of GET, UPDATE, DELETE API functionality

--- app/http/controllers/user.js
+++ app/http/controllers/user.js
@@ -2,28 +2,98 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+var UserHandle    = require("../../models/user")
+
 
 exports.create = function (req, res) { 
-	var Users    = require("../../models/user")
-	  , userInfo = req.body
-	  , data     = {};
+	var userInfo = req.body
+	  , out      = {
+	  	error: null,
+	  	displayName: null
+	  };
 
 	userInfo._id = userInfo.email;
 
-	var user = new Users(userInfo);
+	var user = new UserHandle(userInfo);
 
 	console.log("userInfo")
 
 	user.save( function(err, thisUser) {
 		if (err) {
-			data.error = err;
+			out.error = err;
 		}
 		else {
-			data.error = null;
-			data.displayName = thisUser.displayName;
+			out.displayName = thisUser.displayName;
 		}
-		res.send(data);
+		res.send(out);
 	});
+};
+
+exports.get = function (req, res) { 
+	var id   = req.params.id
+	  , out  = {};
+
+	UserHandle.find( { _id : id }, function (err, user) {
+		if (!user.length || err) {
+			// TODO: Robust error reporting
+			out.error = err || "User not found. That's strange!";
+		}
+		else {
+			out.data = user[0];
+		}
+		res.send(out);
+	})
+};
+
+exports.update = function (req, res) {
+	// Overwrites all information, and thus expects all information
+
+	// TODO: Examine client-side implications of this approach,
+	//       including nested return information (see [out] var definition 
+    //       below)
+
+	var userInfo = req.body
+	  , id = req.params.id
+	  , out      = {
+	  	user: userInfo,
+	  	error: null
+	  };
+
+	// TODO: Check if the :id in the path is necessary
+	UserHandle.update( { _id = id}, userInfo, function (err, num, raw) {
+		if (err || !num) {
+			// TODO: Robust error reporting
+			out.error = err || "User not found. That's strange!";
+		}
+
+		req.send(out);
+	}); 
+}
+
+exports.del = function (req, res) {
+	var id = req.params.id
+	  , out = {
+	  	error: null,
+	  	success: true;
+	  }
+
+	// Find and remove
+	UserHandle.findByIdAndRemove(id , function (user) {
+		if (!user || !user.length) {
+			// TODO: Robust error reporting
+			out.error = "User not found. That's strange!";
+			out.success = false;
+		} else {
+			// Cascading content deletion logic here
+			// TODO: Investigate cascading purge of user content
+		}
+
+		res.send(out);
+	});
+};
+
+exports.suspend = function (req, res) {
+	
 };
 
 exports.userForm = function(req,res) {
--- app/http/routes.js
+++ app/http/routes.js
@@ -9,7 +9,13 @@ module.exports = function(http){
     http.get('/signin', routes.site.signin);
     http.get('/js/sso.js', routes.site.sso);
     http.get('/ajax/forms/new_user.html', routes.user.userForm);
+    
     http.post('/user/create', routes.user.create);
+    http.put('/user/:id/update', routes.user.update);
+    http.get('/user/:id/profile', routes.user.get);
+    http.del('/user/:id/delete', routes.user.del);
+    // http.put('/user/:id/suspend', routes.user.suspend);
+
     // delete this as soon as we hit prod, PLEASE!!
     http.get('/dev/delete', routes.user.devDelete);
 };
